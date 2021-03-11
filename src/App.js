1cd0ead9ff6926fd37adbe33c7a09137ff306832
rahulvirani03 <rahulsvirani25@gmail.com>
2021-03-11 13:11:03 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+import "./App.css";
+import CovidApp from "./components/CovidApp";
+
+class App extends Component {
+  
+
+  
+
+  render() {
+
+    return (
+      <CovidApp>
+      </CovidApp>
+    );
+  }
+}
+
+export default App;
